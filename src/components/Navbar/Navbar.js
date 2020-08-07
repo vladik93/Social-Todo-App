6403c7e01804012fcadb7cd62f07c15ea8c1958b
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-08-07 22:51:28 +0300

    made a pretty good looking navbar & came up with a clever (I think) name

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,53 @@
+import './Navbar.css';
 import React from 'react';
 
-import './Navbar.css';
+// Material-Ui Components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+// Material-Ui Styles
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+    },
+    toolbar: {
+        background: 'linear-gradient(90deg, #006666 30%, #33334d 90%)',
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1)
+    },
+    menuButton: {
+        marginRight: theme.spacing(0)
+    }
+}));
 
 function Navbar() {
+    const classes = useStyles();
     return (
-        <div>Navbar</div>
+        <div className={classes.root}>
+            <AppBar position="fixed">
+                <Toolbar className={classes.toolbar}>
+                    <IconButton color="inherit" className={classes.menuButton}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        DO-TOO!
+                    </Typography>
+                    <IconButton color="inherit">
+                        <AccountCircle />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
