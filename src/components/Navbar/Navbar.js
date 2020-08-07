e5aa9ffb63ba2a21fa962216897140e85d909f8f
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-08-07 10:23:35 +0300

    cleaned up again and installed material-ui

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,14 +4,8 @@ import './Navbar.css';
 
 function Navbar() {
     return (
-        <div className="Navbar">
-           <div className="logo-grid">
-                <i class="item-1 fas fa-tasks"></i>
-                <h2 className="item-2">MyTasks</h2>
-           </div>
-           <a href="#">Login</a>
-        </div>
-    );
+        <div>Navbar</div>
+    )
 }
 
 export default Navbar;
