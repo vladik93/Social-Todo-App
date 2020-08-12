bfb7f8a8fa3d4af48e743d73fc3b1cfcc423d50e
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-08-12 17:28:45 +0300

    added a card to the task component (seems okay with the look of the site)

--- src/components/Tasks/CurrentTasks.js
+++ src/components/Tasks/CurrentTasks.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles(() => ({
+
+
+}));
 
 function CurrentTasks() {
+    const classes = useStyles();
     return (
-        <div>Current Tasks</div>
+        <div className={classes.root}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Card variant="outlined">
+                        <CardHeader 
+                        avatar={
+                            <Avatar>
+                                S
+                            </Avatar>  
+                        }>
+                        </CardHeader>
+                        <CardContent>
+                            Card #1
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
