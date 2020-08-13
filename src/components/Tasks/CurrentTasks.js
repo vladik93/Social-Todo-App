9a74499fb2e0bde50f876de312c50796dcde21ac
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-08-13 10:26:33 +0300

    added stylized cards for the current tasks

--- src/components/Tasks/CurrentTasks.js
+++ src/components/Tasks/CurrentTasks.js
@@ -6,24 +6,68 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography';
 import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const useStyles = makeStyles(() => ({
-
+const useStyles = makeStyles((theme) => ({
+    card: {
+        margin: theme.spacing(1)
+    },
+    cardActions: {
+        // flexGrow: 2,
+        justifyContent: 'space-between'
+    }
 
 }));
 
 function CurrentTasks() {
     const classes = useStyles();
+
+    const tasks = [
+        {id: 1, title: 'Get Super Fit', subtasks: ['Do cardio', 'Eat right', 'Don\'t succumb to temptation'], date: '12/05/20'},
+        {id: 2, title: 'Go On Vacation', subtasks: ['Wait til COVID-19 stops', 'Plan out whole trip',  'Post useless shit on social media'], date: '21/06/20'},
+        {id: 3, title: 'Visit Dying Parents', subtasks: ['Visit parents', 'Deal with horrifying grief', 'Don\'t visit for at least 5 years'], date: '10/06/20'}
+    ];
+
     return (
         <div className={classes.root}>
             <Grid container>
-                <Grid item xs={12}>
+                {tasks.map((task, index) => (
+                    <Grid item xs={12} sm={6}>
+                        <Card variant="elevation" className={classes.card}>
+                            <CardHeader avatar={
+                                <Avatar src="/images/profiles/profile1.jpg"></Avatar>
+                            }
+                            title={task.title}
+                            subheader={task.date}>
+                            
+                            </CardHeader>
+                            <CardActions className={classes.cardActions}>
+                                <IconButton>
+                                    <ExpandMoreIcon />
+                                </IconButton>
+                                    <AvatarGroup max={3} spacing="small">
+                                        <Avatar src="/images/profiles/profile1.jpg" />
+                                        <Avatar src="/images/profiles/profile1.jpg" />
+                                        <Avatar src="/images/profiles/profile1.jpg" />
+                                        <Avatar src="/images/profiles/profile1.jpg" />
+                                        <Avatar src="/images/profiles/profile1.jpg" />
+                                    </AvatarGroup>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+
+
+                {/* <Grid item xs={12}>
                     <Card variant="outlined">
                         <CardHeader 
                         avatar={
                             <Avatar>
-                                S
+                                Cu
                             </Avatar>  
                         }>
                         </CardHeader>
@@ -31,7 +75,7 @@ function CurrentTasks() {
                             Card #1
                         </CardContent>
                     </Card>
-                </Grid>
+                </Grid> */}
             </Grid>
         </div>
     )
