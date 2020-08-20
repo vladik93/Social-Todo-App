b3df79dac72687f5dbd99dc3c433f97272f4d532
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-08-20 14:03:11 +0300

    allowed adding an extra field on click for the multistep-tasks

--- src/components/Tasks/TaskTypes/MultistepTask.js
+++ src/components/Tasks/TaskTypes/MultistepTask.js
@@ -1,28 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 function MultistepTask() {
     const [ taskQuantity, setTaskQuantity] = useState(1);
+    let tasks = [];
 
-    const onMultitaskInit = () => {
-        let rows = [];
-        for(let i = 1; i <= taskQuantity; i++) {
-           rows.push(
-               <Grid item xs={12}>
-                    <TextField variant="outlined" margin="normal" fullWidth placeholder={`Task ${i}`} />
-               </Grid>
-           )
+    const setMultitaskPanels = () => {
+     
+        for(let i = 0; i < taskQuantity; i++) {
+            tasks.push(
+                <Grid item xs={12}>
+                    <TextField variant="outlined" margin="normal" placeholder="Task..." fullWidth />
+                </Grid>
+            )
         }
-        return rows;
+        return tasks;
     }
+
+
 
     return (
         <React.Fragment>
             <Grid item xs={12}>
                 <TextField variant="outlined" margin="normal" placeholder="Title..." multiline rows="3" fullWidth />
             </Grid>
-            {onMultitaskInit()}
+            {setMultitaskPanels()}
+            <IconButton onClick={() => setTaskQuantity(taskQuantity + 1)}>
+                <AddCircleOutlineIcon />
+            </IconButton>
+            
+            {/* <IconButton onClick={() => taskQuantity < 3 ? setTaskQuantity(taskQuantity + 1) : setTaskQuantity(taskQuantity)}>
+                <AddCircleOutlineIcon />
+            </IconButton>
+             */}
         </React.Fragment>
     )
 }
